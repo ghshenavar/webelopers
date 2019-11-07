8322cedd678e946826e3eaea4229bc3687ecb664
dorna <dornadehghani@ce.sharif.edu>
2019-11-07 17:01:09 +0330

    x

--- webelopers/views.py
+++ webelopers/views.py
@@ -21,25 +21,11 @@ def registering(request):
 			login(request, user)
 			return redirect('home')
 	else:
-		form = UserCreationForm()
+		form = SignUpForm()
 
 	return render(request, 'register.html', {'form': form})
 
 
 def login(request):
+
 	return render(request, 'login.html', {'form': form})
-
-
-def signup(request):
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data.get('username')
-			raw_password = form.cleaned_data.get('password1')
-			user = authenticate(username=username, password=raw_password)
-			login(request, user)
-			return redirect('home')
-	else:
-		form = SignUpForm()
-	return render(request, 'signup.html', {'form': form})
