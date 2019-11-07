1e4ea19010f4c8afa052fcdcf2b6dd34e01da278
ghshenavar <ghshenavar@gmail.com>
2019-11-07 16:44:23 +0330

    user creation

--- webelopers/forms.py
+++ webelopers/forms.py
@@ -7,6 +7,9 @@ class SignUpForm(UserCreationForm):
 	first_name = forms.CharField(max_length=30)
 	last_name = forms.CharField(max_length=30)
 	email = forms.EmailField(max_length=254)
+	username = forms.CharField(max_length=100)
+	password1 = forms.CharField(max_length=100)
+	password2 = forms.CharField(max_length=100)
 
 	class Meta:
 		model = User
--- webelopers/views.py
+++ webelopers/views.py
@@ -16,12 +16,12 @@ def registering(request):
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
-			raw_password = form.cleaned_data.get('password1')
+			raw_password = form.cleaned_data.get('password')
 			user = authenticate(username=username, password=raw_password,)
-			login(request, user)
-			return redirect('home')
+			#login(request, user)
+			return redirect('/')
 	else:
-		form = UserCreationForm()
+		form = SignUpForm()
 
 	return render(request, 'register.html', {'form': form})
 
