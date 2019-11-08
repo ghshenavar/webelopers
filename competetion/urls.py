e0f7942a2dcde9cdc8d313343422bcbd9e59c0d8
dorna <dornadehghani@ce.sharif.edu>
2019-11-08 14:45:11 +0330

    x

--- competetion/urls.py
+++ competetion/urls.py
@@ -20,16 +20,16 @@ from django.conf.urls.static import static
 from webelopers.views import *
 
 urlpatterns = [
-				  path('admin/', admin.site.urls),
-				  path('', index, name='homePage'),
-				  path('register/', registering, name='register'),
-				  path('contact/', contact, name='contact_us'),
-				  path('contact/confirmed/', confirmation, name='confirmed'),
-				  path('login/', Login, name='login'),
-				  path('logout/', logout_view, name='logout'),
-				  path('profile/', profile, name='profile'),
-				  path('panel/', panel, name='panel'),
-				  path('edit/', edit, name='edit')
-			  ]
+	path('admin/', admin.site.urls),
+	path('', index, name='homePage'),
+	path('register/', registering, name='register'),
+	path('contact/', contact, name='contact_us'),
+	path('contact/confirmed/', confirmation, name='confirmed'),
+	path('login/', Login, name='login'),
+	path('logout/', logout_view, name='logout'),
+	path('profile/', profile, name='profile'),
+	path('panel/', panel, name='panel'),
+	path('edit/', edit, name='edit')
+]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
